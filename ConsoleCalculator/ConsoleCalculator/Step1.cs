0927d8998d506334eae8c8960a78ebd4449c8dc4
Kan, Seijiro <skan@jbs.com>
2022-03-18 11:30:59 +0900

    コンソールアプリ（計算機）

---
+++ ConsoleCalculator/ConsoleCalculator/Step1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
+{
+    public class Step1
+    {
+        public int Run(string input1, string input2, string ope)
+        {
+            var number1 = int.Parse(input1);
+            var number2 = int.Parse(input2);
+
+            var result = Calc(number1, number2, ope);
+
+            return result;
+        }
+
+        private int Calc(int number1, int number2, string ope) => ope switch
+        {
+            "+" => number1 + number2,
+            "-" => number1 - number2,
+            "*" => number1 * number2,
+            "/" => number1 / number2,
+            _ => throw new ArgumentException("演算可能な演算子を指定してください。"),
+        };
+    }
+}
---
+++ ConsoleCalculator/ConsoleCalculatorTests/Step1Tests.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator.Tests
+{
+    [TestClass()]
+    public class Step1Tests
+    {
+        [TestMethod()]
+        public void RunTest_加算()
+        {
+            Step1 step1 = new Step1();
+            var input1 = "8";
+            var input2 = "2";
+            var ope = "+";
+
+            var actual = step1.Run(input1, input2, ope);
+            Assert.AreEqual(10, actual);
+        }
+
+        [TestMethod()]
+        public void RunTest_減算()
+        {
+            Step1 step1 = new Step1();
+            var input1 = "8";
+            var input2 = "2";
+            var ope = "-";
+
+            var actual = step1.Run(input1, input2, ope);
+            Assert.AreEqual(6, actual);
+        }
+
+        [TestMethod()]
+        public void RunTest_乗算()
+        {
+            Step1 step1 = new Step1();
+            var input1 = "8";
+            var input2 = "2";
+            var ope = "*";
+
+            var actual = step1.Run(input1, input2, ope);
+            Assert.AreEqual(16, actual);
+        }
+
+        [TestMethod()]
+        public void RunTest_除算()
+        {
+            Step1 step1 = new Step1();
+            var input1 = "8";
+            var input2 = "2";
+            var ope = "/";
+
+            var actual = step1.Run(input1, input2, ope);
+            Assert.AreEqual(4, actual);
+        }
+
+        [Ignore]
+        [TestMethod()]
+        public void RunTest_累乗()
+        {
+            Step1 Step1 = new Step1();
+            var input1 = "8";
+            var input2 = "2";
+            var ope = "^";
+
+            var actual = Step1.Run(input1, input2, ope);
+            Assert.AreEqual(64, actual);
+        }
+
+        [Ignore]
+        [TestMethod()]
+        public void RunTest_剰余()
+        {
+            Step1 Step1 = new Step1();
+            var input1 = "8";
+            var input2 = "2";
+            var ope = "%";
+
+            var actual = Step1.Run(input1, input2, ope);
+            Assert.AreEqual(0, actual);
+        }
+
+    }
+}
